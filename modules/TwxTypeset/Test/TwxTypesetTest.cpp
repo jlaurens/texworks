b49fbe5072135ba1bd3c23258ec06c61f8f7e99e
LAURENS Jérôme <jerome.laurens@u-bourgogne.fr>
2023-06-02 16:54:14 +0200

    Update TwxTypesetTest.cpp

--- modules/TwxTypeset/Test/TwxTypesetTest.cpp
+++ modules/TwxTypeset/Test/TwxTypesetTest.cpp
@@ -19,22 +19,14 @@
 	see <http://www.tug.org/texworks/>.
 */
 
-#include "TwxCoreLibTest.h"
-
-#include <TwxConst.h>
-
-// #include "TwxTool.h"
-// #include "TwxInfo.h"
-// #include "TwxLocate.h"
-// #include "TwxAssetsTrackDB.h"
-// #include "TwxAssets.h"
-// #include "TwxSettings.h"
+#include "TwxTypesetTest.h"
+#include "TwxTypesetManager.h"
 
 #include <QFileInfo>
 #include <QDir>
 
 namespace Twx {
-namespace Core {
+namespace Typeset {
 
 namespace Test {
 
@@ -67,12 +59,150 @@ void Main::cleanup()
 {
 }
 
+const QString empty;
+const auto fileA = QStringLiteral("a");
+const auto fileB = QStringLiteral("b");
+
+void Main::test_empty()
+{
+	QVERIFY(Manager::getOwnerForRootFile(empty) == nullptr);
+	QVERIFY(!Manager::isFileBeingTypeset(empty));
+	QVERIFY(Manager::getOwnerForRootFile(fileA) == nullptr);
+	QVERIFY(!Manager::isFileBeingTypeset(fileA));
+	QVERIFY(Manager::getOwnerForRootFile(fileB) == nullptr);
+	QVERIFY(!Manager::isFileBeingTypeset(fileB));
+}
+
 void Main::test()
 {
+	QObject owner;
+
+	QSignalSpy started(Manager::emitter(), &Manager::typesettingStarted);
+	QSignalSpy stopped(Manager::emitter(), &Manager::typesettingStopped);
+
+	QVERIFY(started.isValid());
+	QVERIFY(stopped.isValid());
+
+	// 2a) start process
+	QCOMPARE(started.count(), 0);
+	QCOMPARE(stopped.count(), 0);
+	QCOMPARE(Manager::test_count,0);
+	QVERIFY(Manager::startTypesetting(fileA, &owner));
+	QCOMPARE(Manager::test_count,0);
+	QCOMPARE(started.count(), 1);
+	QCOMPARE(started.takeFirst().at(0).toString(), fileA);
+	QCOMPARE(stopped.count(), 0);
+
+	// 2b) one process running
+	QCOMPARE(Manager::getOwnerForRootFile(empty),nullptr);
+	QVERIFY(!Manager::isFileBeingTypeset(empty));
+	QCOMPARE(Manager::getOwnerForRootFile(fileA), &owner);
+	QVERIFY(Manager::isFileBeingTypeset(fileA));
+	QVERIFY(Manager::getOwnerForRootFile(fileB) == nullptr);
+	QVERIFY(!Manager::isFileBeingTypeset(fileB));
+
+	// 2c) Can't start again
+	QCOMPARE(started.count(), 0);
+	QCOMPARE(stopped.count(), 0);
+	QVERIFY(!Manager::startTypesetting(fileA, &owner));
+	QCOMPARE(started.count(), 0);
+	QCOMPARE(stopped.count(), 0);
+
+	// 2d) Can't start with invalid file
+	QVERIFY(!Manager::startTypesetting(empty, &owner));
+	QCOMPARE(started.count(), 0);
+	QCOMPARE(stopped.count(), 0);
+
+	// 2e) Can't start with invalid window
+	QVERIFY(!Manager::startTypesetting(fileB, nullptr));
+	QCOMPARE(started.count(), 0);
+	QCOMPARE(stopped.count(), 0);
+
+	// 2f) still one process running
+	QCOMPARE(Manager::getOwnerForRootFile(empty), nullptr);
+	QVERIFY(!Manager::isFileBeingTypeset(empty));
+	QCOMPARE(Manager::getOwnerForRootFile(fileA), &owner);
+	QVERIFY(Manager::isFileBeingTypeset(fileA));
+	QCOMPARE(Manager::getOwnerForRootFile(fileB), nullptr);
+	QVERIFY(!Manager::isFileBeingTypeset(fileB));
+
+	// 3a) Start second process but destroy window
+	{
+		QObject owner2;
+		QCOMPARE(started.count(), 0);
+		QCOMPARE(stopped.count(), 0);
+		QVERIFY(Manager::startTypesetting(fileB, &owner2));
+		QCOMPARE(started.count(), 1);
+		QCOMPARE(started.takeFirst().at(0).toString(), fileB);
+		QCOMPARE(stopped.count(), 0);
+
+		// two processes running
+		QCOMPARE(Manager::getOwnerForRootFile(empty), nullptr);
+		QVERIFY(!Manager::isFileBeingTypeset(empty));
+		QCOMPARE(Manager::getOwnerForRootFile(fileA), &owner);
+		QVERIFY(Manager::isFileBeingTypeset(fileA));
+		QCOMPARE(Manager::getOwnerForRootFile(fileB), &owner2);
+		QVERIFY(Manager::isFileBeingTypeset(fileB));
+
+		// 3b) destroying the typesetting window (at the end of the scope) should stop
+		QCOMPARE(started.count(), 0);
+		QCOMPARE(stopped.count(), 0);
+	}
+	QCOMPARE(started.count(), 0);
+	QCOMPARE(stopped.count(), 1);
+	QCOMPARE(stopped.takeFirst().at(0).toString(), fileB);
+	QCOMPARE(Manager::test_count,1);
+	Manager::test_count = 0;
+
+	// 3c) Start second process but destroy window
+	{
+		QObject owner2;
+		QVERIFY(Manager::startTypesetting(fileB, &owner2));
+		QCOMPARE(started.count(), 1);
+		QCOMPARE(started.takeFirst().at(0).toString(), fileB);
+		QCOMPARE(stopped.count(), 0);
+		Manager::stopTypesetting(&owner2);
+		QCOMPARE(started.count(), 0);
+		QCOMPARE(stopped.count(), 1);
+		QCOMPARE(stopped.takeFirst().at(0).toString(), fileB);
+		QVERIFY(Manager::startTypesetting(fileB, &owner2));
+		QCOMPARE(started.count(), 1);
+		QCOMPARE(started.takeFirst().at(0).toString(), fileB);
+		QCOMPARE(stopped.count(), 0);
+	}
+	QCOMPARE(Manager::test_count,1);
+	Manager::test_count = 0;
+	QCOMPARE(started.count(), 0);
+	QCOMPARE(stopped.count(), 1);
+	QCOMPARE(stopped.takeFirst().at(0).toString(), fileB);
+
+	// 3c) one process running
+	QCOMPARE(Manager::getOwnerForRootFile(empty), nullptr);
+	QVERIFY(!Manager::isFileBeingTypeset(empty));
+	QCOMPARE(Manager::getOwnerForRootFile(fileA), &owner);
+	QVERIFY(Manager::isFileBeingTypeset(fileA));
+	QCOMPARE(Manager::getOwnerForRootFile(fileB), nullptr);
+	QVERIFY(!Manager::isFileBeingTypeset(fileB));
+
+	// 4a) stop process
+	QCOMPARE(started.count(), 0);
+	QCOMPARE(stopped.count(), 0);
+	Manager::stopTypesetting(&owner);
+	QCOMPARE(started.count(), 0);
+	QCOMPARE(stopped.count(), 1);
+	QCOMPARE(stopped.takeFirst().at(0).toString(), fileA);
+
+	// 3b) no process running
+	QCOMPARE(Manager::getOwnerForRootFile(empty), nullptr);
+	QVERIFY(!Manager::isFileBeingTypeset(empty));
+	QCOMPARE(Manager::getOwnerForRootFile(fileA), nullptr);
+	QVERIFY(!Manager::isFileBeingTypeset(fileA));
+	QCOMPARE(Manager::getOwnerForRootFile(fileB), nullptr);
+	QVERIFY(!Manager::isFileBeingTypeset(fileB));
 }
 
 } // namespace Test
 } // namespace Core
 } // namespace Twx
 
-QTEST_MAIN(Twx::Core::Test::Main)
+QTEST_MAIN(Twx::Typeset::Test::Main)
